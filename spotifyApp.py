0c0c7c1fdcd0d3c02bf3f8022e275fcf2ff8a288
Daniel Warren <danielwarren@Daniels-MacBook-Pro.local>
2024-04-09 16:00:46 -0500

    getauthcode

--- spotifyApp.py
+++ spotifyApp.py
@@ -1,40 +1,61 @@
-import base64
 from tkinter import *
+from PyQt5.QtWidgets import QApplication
+import random
+import string
 import webbrowser
+import base64
 
+import requests
 root = Tk()
+page = 'Start'
 
 def getAuthCode():
-    def getFromTextBar():
+    def getCode():
         hold = e.get()
-        print(hold)
-        i = hold.index('code=') + 5
-        j = hold.index('&')
-        code = hold[i:j]
-        
-        encode = base64.urlsafe_b64encode((c + ':' + s).encode())
-        headers = {
-            'Authorization': 
+        i = hold.find('code=')
+        j = hold.find('&')
+        code = hold[i + 5: j]
+        print(hold + '\n' + code)
+        e.destroy()
+        b.destroy()
+        getAuthItems(code)
+
+    def getAuthItems(code):
+        body = {
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': redirect
         }
-        
+
+        headers = {
+            'Authorization': 'Basic ' + encode,
+            'content-type': 'application/x-www-form-urlencoded'
+        }
+        url = 'https://accounts.spotify.com/api/token'
+        r = requests.post(url=url, headers=headers, params=body)
+        print(r.text)
 
     global authItems
-    c = "bf9fd0131e0a4c56ab5ec69dc87befc3"
-    s = "9ab7c1a94f2841d6bb97102680edd97d"
-    redirect = "http://localhost:80"
-    scope = "user-read-private,user-library-read,user-library-modify,user-modify-playback-state,user-read-playback-state"
-    uri = "https://accounts.spotify.com/api/token"
-    URL = "https://accounts.spotify.com/authorize?client_id=" 
-    URL += c + "&response_type=code&redirect_uri=" + redirect +"&state=abcdefghijklmnop&scope=" + scope
-    webbrowser.open(URL)
-
+    c = 'bf9fd0131e0a4c56ab5ec69dc87befc3'
+    s = '9ab7c1a94f2841d6bb97102680edd97d'
+    redirect = 'http://localhost:80'
+    chars = string.ascii_lowercase + string.digits
+    state = ''.join(random.choice(chars) for i in range(16))
+    scope = 'user-read-private,user-read-email'
+    type = 'code'
+    url = f'http://accounts.spotify.com/authorize?response_type={type}'
+    url = url + f'&client_id={c}&scope={scope}'
+    url = url + f'&redirect_uri={redirect}&state={state}'
+    webbrowser.open(url)
+    encode = base64.b64encode(f'{c}:{s}')
     start.destroy()
-    e = Entry(root, width=100)
+    e = Entry(root, text='Paste Address Here')
+    b = Button(root, text='Confirm', command=getCode)
     e.pack()
-    e.insert(0, 'Paste contents from Address Bar Here')
-    b = Button(root, text='Confirm', command=getFromTextBar)
     b.pack()
+
 
 start = Button(root, text='Click to Start', command=getAuthCode)
 start.pack()
+
 root.mainloop()
