c808aa5fe5616902ae117143b1fd40e6d05eeaf3
Daniel Warren <danielwarren@Daniels-MacBook-Pro.local>
2024-04-10 12:15:21 -0500

    Added to Main Screen

--- spotifyApp.py
+++ spotifyApp.py
@@ -1,3 +1,6 @@
+import datetime
+import os
+import time
 from tkinter import *
 import random
 import string
@@ -8,17 +11,49 @@ import requests
 
 authItems = {}
 
+
+class pickPlaylist():
+    def __init__(self, frame, nextPage):
+        url = 'https://api.spotify.com/v1/me/playlists'
+        auth = authItems['type'] + ' ' + authItems['accessTok']
+        headers = {
+            'Authorization': auth
+        }
+        r = requests.get(url=url, headers=headers)
+        x = json.loads(r.text)
+        items = x['items']
+        for i in items:
+            
+
 class start():
     def __init__(self, frame):
         b = Button(frame, text='Click to Start', command=lambda: self.startProgram(b))
         b.pack()
     def startProgram(self, start):
+        global authItems
+        f = open('AuthItems.json', 'r')
+        if os.stat('/Users/danielwarren/Desktop/Spotify_App/AuthItems.json').st_size != 0:
+            x = json.load(f)
+            a = x['Expire_Time']
+            check = datetime.datetime.strptime(a, '%Y-%m-%d %H:%M:%S.%f')
+            if datetime.datetime.now() < check:
+                authItems = {
+                    'accessTok': x['accessTok'],
+                    'type': x['type'],
+                    'expires_in': x['expires_in'],
+                    'refreshTok': x['refreshTok'],
+                    'scope': x['scope'], 
+                    'Access_Time': x['Access_Time'],
+                    'Expire_Time': x['Expire_Time']
+                }
+                con.pickPage('Main', frame)
+                return
         c = 'bf9fd0131e0a4c56ab5ec69dc87befc3'
         s = '9ab7c1a94f2841d6bb97102680edd97d'
         redirect = 'http://localhost:80'
         chars = string.ascii_lowercase + string.digits
         state = ''.join(random.choice(chars) for i in range(16))
-        scope = 'user-read-private,user-read-email'
+        scope = 'user-read-private,user-read-email,playlist-read-private'
         type = 'code'
         url = f'http://accounts.spotify.com/authorize?response_type={type}'
         url = url + f'&client_id={c}&scope={scope}'
@@ -56,29 +91,31 @@ class start():
         url = 'https://accounts.spotify.com/api/token'
         r = requests.post(url=url, headers=headers, params=body)
         x = json.loads(r.text)
+        now = datetime.datetime.now()
+        expTime = str(now + datetime.timedelta(seconds=3600))
+        now = str(now)
         authItems = {
             'accessTok': x['access_token'],
             'type': x['token_type'],
             'expires_in': x['expires_in'],
             'refreshTok': x['refresh_token'],
-            'scope': x['scope']
+            'scope': x['scope'], 
+            'Access_Time': now,
+            'Expire_Time': expTime
         }
+
+        f = open('AuthItems.json', 'w')
+        json.dump(authItems, f, indent=4)
         con.pickPage('Main', frame)
 
 class MainScreen():
     def __init__(self, frame):
-        global a
-        global b
-        a = Button(frame, text='Idk man, Im just vibin', command=self.check)
-        a.pack()
-        b = Button(frame, text='Quit', command=lambda: self.pickPage('Quit'))
-        b.pack()
-    def pickPage(self, page):
-        a.destroy()
-        b.destroy()
-        con.pickPage(page, frame)
-    def check(self):
-        print('Hello')
+        cleanPlayDate = Button(frame, text='Clean a Playlist By Date', command=lambda: con.pickPage('cleanPlayDate', frame))
+        cleanPlayArt = Button(frame, text='Clean a Playlist By Artist', command=lambda: con.pickPage('cleanPlayArt', frame))
+        cleanPlayUser = Button(frame, text='Clean a Playlist By Tracks You Pick', command=lambda: con.pickPage('cleanPlayUser', frame))
+        cleanPlayDate.pack()
+        cleanPlayArt.pack()
+        cleanPlayUser.pack()
 
 class Quit():
     def __init__(self, frame):
@@ -95,14 +132,29 @@ class Controller():
         if page == 'Start':
             start(frame)
         elif page == 'Main':
+            self.clearFrame(frame)
             root.title('Main')
             m = MainScreen(frame)
+        elif page == 'cleanPlayDate':
+            self.clearFrame(frame)
+            n = pickPlaylist(frame, page)
+        elif page == 'cleanPlayArt':
+            self.clearFrame(frame)
+            n = pickPlaylist(frame, page)
+        elif page == 'cleanPlayUser':
+            self.clearFrame(frame)
+            n = pickPlaylist(frame, page)
         elif page == 'Quit':
+            self.clearFrame(frame)
             root.title('Quit')
             Quit(frame)
             print(authItems)
+    def clearFrame(self, frame):
+        for i in frame.winfo_children():
+            i.destroy()
 root = Tk()
 root.title('Start')
+root.geometry('500x500')
 con = Controller()
 frame = Frame(root)
 frame.pack()
